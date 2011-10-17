72fbc28d17e37139ef43389e020c54f678cf17b9
Ilya Razenshteyn <ilyaraz@gmail.com>
2011-10-18 00:53:03 +0400

    held_karp.cpp -> hk.cpp (so to avoid collision with held_karp.h)

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-all: held_karp
-held_karp: held_karp.cpp held_karp.h utils.h
-	g++	held_karp.cpp -o held_karp -Wall -O3 -lgurobi45 -lemon
+all: hk
+hk: hk.cpp held_karp.h utils.h
+	g++	hk.cpp -o hk -Wall -O3 -lgurobi45 -lemon
 clean:
-	rm -rf held_karp
+	rm -rf hk
--- scripts/test_held_karp.sh
+++ scripts/test_held_karp.sh
@@ -6,6 +6,6 @@ do
     touch ../logs/log$i.txt
     for ((j = 0; j < 50; ++j))
     do
-        python ../generators/random_euclidean.py $i | ../held_karp 2>>../logs/log$i.txt
+        python ../generators/random_euclidean.py $i | ../hk 2>>../logs/log$i.txt
     done
 done

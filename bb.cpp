cca1438152287df00778687798065a25a94abea4
Ilya Razenshteyn <ilyaraz@gmail.com>
2012-01-19 02:36:30 +0900

    outputting generated points

--- bb.cpp
+++ bb.cpp
@@ -1,6 +1,7 @@
 #include "branch_bound.h"
 #include "utils.h"
 
+#include <fstream>
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -19,5 +20,12 @@ int main(int argc, char **argv) {
     std::vector<std::vector<double> > metric = tsp::utils::getEuclideanMetric(points);
     tsp::core::BranchBound bb(metric);
     bb.run();
+    std::ofstream output("points.txt");
+    output << n << std::endl;
+    output.setf(std::ios::fixed);
+    output.precision(20);
+    for (int i = 0; i < n; ++i) {
+        output << points[i].first << " " << points[i].second << std::endl;
+    }
     return 0;
 }
